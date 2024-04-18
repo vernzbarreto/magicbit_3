bcd1a55037bf8fba55e936ebb93a02482636ea34
vernz <kariowl888@gmail.com>
2024-04-19 00:15:21 +0800

    main chnge

--- main.ts
+++ main.ts
@@ -5,47 +5,15 @@ load dependency
 "magicbit": "file:../pxt-magicbit"
 */
 
-enum RgbColors {
-        //% block=red
-        Red = 0xFF0000,
-        //% block=orange
-        Orange = 0xFFA500,
-        //% block=yellow
-        Yellow = 0xFFFF00,
-        //% block=green
-        Green = 0x00FF00,
-        //% block=blue
-        Blue = 0x0000FF,
-        //% block=indigo
-        Indigo = 0x4b0082,
-        //% block=violet
-        Violet = 0x8a2be2,
-        //% block=purple
-        Purple = 0xFF00FF,
-        //% block=white
-        White = 0xFFFFFF,
-        //% block=black
-        Black = 0x000000
-}
-
-enum RgbUltrasonics {
-    //% block=left
-    Left = 0x00,
-    //% block=right
-    Right = 0x01,
-    //% block=all
-    All = 0x02
-}
-
-enum ColorEffect {
-    //% block=none
-    None = 0x00,
-    //% block=breathing
-    Breathing = 0x01,
-    //% block=rotate
-    Rotate = 0x02,
-    //% block=flash
-    Flash = 0x03
+enum Offset {
+    //% block=one
+    ONE = 0,
+    //% block=two
+    TWO = 1,
+    //% block=three
+    THREE = 2,
+    //% block=four
+    FOUR = 3
 }
 
 //% color="#EE6A50" weight=10 icon="\uf013"
@@ -124,9 +92,6 @@ namespace magicbit {
     }
 
     let initialized = false
-    let neoStrip: neopixel.Strip;
-    let matBuf = pins.createBuffer(17);
-    let distanceBuf = 0;
 
     function i2cwrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
@@ -227,9 +192,9 @@ namespace magicbit {
      * @param index Servo Channel; eg: S1
      * @param degree [0-180] degree of servo; eg: 0, 90, 180
     */
-    //% blockId=magicbit_servo block="Servo|%index|degree %degree=protractorPicker"
+    //% blockId=magicbit_servo block="Servo|%index|degree %degree"
     //% weight=100
-    //% degree.defl=90
+    //% degree.min=0 degree.max=180
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Servo(index: Servos, degree: number): void {
         if (!initialized) {
@@ -241,17 +206,17 @@ namespace magicbit {
         setPwm(index + 7, 0, value)
     }
 
-/**
-     * Servo Execute
-     * @param index Servo Channel; eg: S1
-     * @param degree1 [0-180] degree of servo; eg: 0, 90, 180
+   /**
+         * Servo Execute
+         * @param index Servo Channel; eg: S1
+         * @param degree1 [0-180] degree of servo; eg: 0, 90, 180
 	 * @param degree2 [0-180] degree of servo; eg: 0, 90, 180
 	 * @param speed [1-10] speed of servo; eg: 1, 10
     */
-    //% blockId=motorbit_servospeed block="Servo|%index|degree start %degree1=protractorPicker|end %degree2=protractorPicker|speed %speed"
+    //% blockId=motorbit_servospeed block="Servo|%index|degree start %degree1|end %degree2|speed %speed"
     //% weight=98
-    //% degree1.defl=90
-    //% degree2.defl=90
+    //% degree1.min=0 degree1.max=180
+    //% degree2.min=0 degree2.max=180
     //% speed.min=1 speed.max=10
     //% inlineInputMode=inline
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
@@ -279,26 +244,26 @@ namespace magicbit {
     }
 
 
-    /**
-     * Geek Servo
-     * @param index Servo Channel; eg: S1
-     * @param degree [-45-225] degree of servo; eg: -45, 90, 225
-    */
-    //% blockId=magicbit_gservo block="Geek Servo|%index|degree %degree=protractorPicker"
-    //% weight=96
-    //% blockGap=50
-    //% degree.defl=90
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function GeekServo(index: Servos, degree: number): void {
-        if (!initialized) {
-            initPCA9685()
-        }
-        // 50hz: 20,000 us
-        let v_us = ((degree - 90) * 20 / 3 + 1500) // 0.6 ~ 2.4
-        let value = v_us * 4096 / 20000
-        setPwm(index + 7, 0, value)
-    }
-
+//     /**
+//      * Geek Servo
+//      * @param index Servo Channel; eg: S1
+//      * @param degree [-45-225] degree of servo; eg: -45, 90, 225
+//     */
+//     //% blockId=magicbit_gservo block="Geek Servo|%index|degree %degree=protractorPicker"
+//     //% weight=96
+//     //% blockGap=50
+//     //% degree.defl=90
+//     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+//     export function GeekServo(index: Servos, degree: number): void {
+//         if (!initialized) {
+//             initPCA9685()
+//         }
+//         // 50hz: 20,000 us
+//         let v_us = ((degree - 90) * 20 / 3 + 1500) // 0.6 ~ 2.4
+//         let value = v_us * 4096 / 20000
+//         setPwm(index + 7, 0, value)
+//     }
+	
     //% blockId=magicbit_stepper_degree block="Stepper 28BYJ-48|%index|degree %degree"
     //% weight=90
     export function StepperDegree(index: Steppers, degree: number): void {
@@ -420,6 +385,7 @@ namespace magicbit {
     //% weight=84
     //% speed1.min=-255 speed1.max=255
     //% speed2.min=-255 speed2.max=255
+    //% inlineInputMode=inline
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function MotorRunDual(motor1: Motors, speed1: number, motor2: Motors, speed2: number): void {
         MotorRun(motor1, speed1);
@@ -460,141 +426,23 @@ namespace magicbit {
         }
     }
 
-    /**
-     * Get RUS04 distance
-     * @param pin Microbit ultrasonic pin; eg: P2
-    */
-    //% blockId=magicbit_ultrasonic block="Read RgbUltrasonic Distance|pin %pin|cm"
-    //% weight=78
-    export function Ultrasonic(pin: DigitalPin): number {
-        return UltrasonicVer(pin, SonarVersion.V1);
-    }
+	//% blockId="motorbit_rus04" block="On-board Ultrasonic part %index show color %rgb effect %effect" 
+	//% weight=78
+	export function motorbit_rus04(index: RgbUltrasonics, rgb: RgbColors, effect: ColorEffect): void {
+	    sensors.board_rus04_rgb(DigitalPin.P16, 4, index, rgb, effect);
+	}
+    
+	//% blockId=Ultrasonic_reading_distance block="On-board Ultrasonic reading distance"
+	//% weight=77
+	export function Ultrasonic_reading_distance(): number {
+	    return sensors.Ultrasonic(DigitalPin.P2);
+	}
 
-    function UltrasonicVer(pin: DigitalPin, v: SonarVersion): number {
 
-        // send pulse
-        if (v == SonarVersion.V1) {
-            pins.setPull(pin, PinPullMode.PullNone);
-        }
-        else { pins.setPull(pin, PinPullMode.PullDown); }
-        pins.digitalWritePin(pin, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(pin, 1);
-        control.waitMicros(50);
-        pins.digitalWritePin(pin, 0);
-
-        // read pulse
-        let d = pins.pulseIn(pin, PulseValue.High, 25000);
-        let ret = d;
-        // filter timeout spikes
-        if (ret == 0 && distanceBuf != 0) {
-            ret = distanceBuf;
-        }
-        distanceBuf = d;
-        if (v == SonarVersion.V1) {
-            return Math.floor(ret * 9 / 6 / 58);
-        }
-        return Math.floor(ret / 40 + (ret / 800));
-        // Correction
-    }
-
-    function RgbDisplay(indexstart: number, indexend: number, rgb: RgbColors): void {
-        for (let i = indexstart; i <= indexend; i++) {
-			neoStrip.setPixelColor(i, rgb);
-		}
-        neoStrip.show();
-    }
-
-	//% blockId="magicbit_rus04" block="RgbUltrasonic|%RgbUltrasonics|show color %rgb|effect %ColorEffect"
+	//% blockId=Setting_the_on_board_lights block="Setting the on-board lights %index color %rgb Effect %effect"
 	//% weight=76
-    export function RUS_04(index: RgbUltrasonics, rgb: RgbColors, effect: ColorEffect): void {
-        let start, end;
-        if (!neoStrip) {
-            neoStrip = neopixel.create(DigitalPin.P16, 10, NeoPixelMode.RGB)
-        }
-        if (index == RgbUltrasonics.Left) {
-            start = 4;
-            end = 6;
-        } else if (index == RgbUltrasonics.Right) {
-            start = 7;
-            end = 9;
-		} else if (index == RgbUltrasonics.All) {
-            start = 4;
-            end = 9;
-		}
-        switch(effect) {
-            case ColorEffect.None:
-                RgbDisplay(start, end, rgb);
-                break;
-            case ColorEffect.Breathing:
-            for (let i = 0; i < 255; i+=2) {
-                neoStrip.setBrightness(i);
-                RgbDisplay(start, end, rgb);
-                //basic.pause((255 - i)/2);
-                basic.pause((i < 20)? 80 :(255/i));
-            }
-            for (let i = 255; i > 0; i-=2) {
-                neoStrip.setBrightness(i);
-                RgbDisplay(start, end, rgb);
-                basic.pause((i < 20)? 80 :(255/i));
-            }
-            break;
-            case ColorEffect.Rotate:
-                for (let i = 0; i < 4; i++) {
-                    neoStrip.setPixelColor(start, rgb);
-                    neoStrip.setPixelColor(start+1, 0);
-                    neoStrip.setPixelColor(start+2, 0);
-                    if (index == RgbUltrasonics.All) {
-                        neoStrip.setPixelColor(end-2, rgb);
-                        neoStrip.setPixelColor(end-1, 0);
-                        neoStrip.setPixelColor(end, 0);
-                    }
-                    neoStrip.show();
-                    basic.pause(150);
-                    neoStrip.setPixelColor(start, 0);
-                    neoStrip.setPixelColor(start+1, rgb);
-                    neoStrip.setPixelColor(start+2, 0);
-                    if (index == RgbUltrasonics.All) {
-                        neoStrip.setPixelColor(end-2, 0);
-                        neoStrip.setPixelColor(end-1, rgb);
-                        neoStrip.setPixelColor(end, 0);
-                    }
-                    neoStrip.show();
-                    basic.pause(150);
-                    neoStrip.setPixelColor(start, 0);
-                    neoStrip.setPixelColor(start+1, 0);
-                    neoStrip.setPixelColor(start+2, rgb);
-                    if (index == RgbUltrasonics.All) {
-                        neoStrip.setPixelColor(end-2, 0);
-                        neoStrip.setPixelColor(end-1, 0);
-                        neoStrip.setPixelColor(end, rgb);
-                    }
-                    neoStrip.show();
-                    basic.pause(150);
-                }
-                RgbDisplay(4, 9, 0);
-                break;
-            case ColorEffect.Flash:
-            for (let i = 0; i < 6; i++) {
-                RgbDisplay(start, end, rgb);
-                basic.pause(150);
-                RgbDisplay(start, end, 0);
-                basic.pause(150);
-            }
-            break;
-        }
-    }
-
-    /**
-     * Init RGB pixels mounted on magicbit
-     */
-    //% blockId="magicbit_rgb" block="RGB"
-    //% weight=75
-    export function rgb(): neopixel.Strip {
-        if (!neoStrip) {
-            neoStrip = neopixel.create(DigitalPin.P16, 10, NeoPixelMode.RGB)
-        }
-        return neoStrip;
-    }
-
+	export function Setting_the_on_board_lights(offset: Offset,rgb: RgbColors, effect: rgb_ColorEffect): void {
+	 sensors.board_rus04_rgb(DigitalPin.P16, offset, 0, rgb, effect);
+	}
+	
 }
